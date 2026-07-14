b12c2747eb7fe8288638d558121b698e59ef9288
Manisha834 <bhattamanisha845@gmail.com>
2026-07-14 11:32:55 +0545

    Updated pages and added images

--- script.js
+++ script.js
@@ -166,4 +166,37 @@ document.addEventListener('DOMContentLoaded',()=>{
 });   // <-- This was missing
 
 
+
+// workshops Card  Javascript
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const buttons = document.querySelectorAll(".filter-btn");
+    const cards = document.querySelectorAll(".workshop-card");
+
+    buttons.forEach(button => {
+
+        button.addEventListener("click", () => {
+
+            buttons.forEach(btn => btn.classList.remove("active"));
+            button.classList.add("active");
+
+            const filter = button.dataset.filter;
+
+            cards.forEach(card => {
+
+                if(filter === "all" || card.dataset.category === filter){
+                    card.style.display = "block";
+                }else{
+                    card.style.display = "none";
+                }
+
+            });
+
+        });
+
+    });
+
+});
+
 })(); // <-- Keep this
